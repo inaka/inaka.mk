49e8d6349185ca0698e119fa51927edaa2aa66e9
Brujo Benavides <elbrujohalcon@inaka.net>
2016-01-05 16:38:15 -0300

    Initial version

---
+++ plugins.mk
@@ -0,0 +1,10 @@
+# Copyright (c) 2016, Erlang Solutions Ltd.
+# This file is part of erlang.mk and subject to the terms of the ISC License.
+
+# Core targets.
+
+help::
+	$(verbose) printf "%s\n" "" \
+		"inaka.mk targets:"
+
+# Plugin-specific targets.
